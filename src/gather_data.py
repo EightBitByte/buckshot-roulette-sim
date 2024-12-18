a836729eeb138140dae95be372f8670dd64aa3a6
EightBitByte <apixelatedpug@gmail.com>
2024-12-17 23:10:17 -0800

    :lipstick: Update tqdm appearance when collecting data

--- src/gather_data.py
+++ src/gather_data.py
@@ -19,7 +19,8 @@ class Experiment:
     num_lives: int
     starting_health: int
 
-# Modify these constants to change the testing parameters/# of trials for averages
+# Modify these constants to change the testing parameters
+# BEGIN CONSTANTS =============================================================
 
 # STRATAGEMS = [Greedy, Safe, Balanced, Random, Risky, Scared]
 STRATAGEMS = list()
@@ -29,25 +30,35 @@ for name, stratagem in getmembers(strat):
     if isclass(stratagem) and name not in ('Stratagem', 'ABC', 'GameState') and not issubclass(stratagem, Enum):
         STRATAGEMS.append(stratagem)
 
+# Number of trials to calculate average win rate
+NUM_TRIALS: int = 100000
 
-NUM_TRIALS: int = 1000
-
+# Experiments to run
 EXPERIMENTS = [
     Experiment(4, 4, 3),
-    # Experiment(2, 4, 3),
-    # Experiment(1, 4, 3),
-    # Experiment(4, 4, 5),
-    # Experiment(2, 4, 5),
-    # Experiment(1, 4, 5),
+    Experiment(2, 4, 3),
+    Experiment(1, 4, 3),
+    Experiment(4, 4, 5),
+    Experiment(2, 4, 5),
+    Experiment(1, 4, 5),
 ]
 
 # Where to output the data generated
 OUT_DIRECTORY = "./data/"
 
+# END CONSTANTS ================================================================
+
 def main() -> None:
-    """Run experiments defined in `EXPERIMENTS`."""
+    """
+    Run experiments defined in `EXPERIMENTS`.
+
+    By default, this function outputs to `./data/[year-month-day]/[hours-minutes]`. 
+    It labels the experiments from `[0, # of experiments - 1]`.
+    For example, a file it would produce would be `./data/2024-12-17/23-02/experiment-0.json`.
+    """
     day: str = datetime.fromtimestamp(time()).strftime('%Y-%m-%d')
-    Path(f'{OUT_DIRECTORY}{day}/').mkdir(parents=True, exist_ok=True)
+    current_time: str = datetime.fromtimestamp(time()).strftime('%H-%M')
+    Path(f'{OUT_DIRECTORY}{day}/{current_time}').mkdir(parents=True, exist_ok=True)
 
     for exp_num, experiment in enumerate(tqdm(EXPERIMENTS, 'Experiments')):
         result_dict: dict = dict()
@@ -57,11 +68,11 @@ def main() -> None:
                                  'trials': NUM_TRIALS}
         result_dict['strats'] = dict()
 
-        for strat in tqdm(STRATAGEMS, 'Strats', colour='dodgerblue'):
+        for strat in tqdm(STRATAGEMS, 'Strats', colour='blue', leave=False):
             result_dict['strats'][strat.__name__.lower()] = dict()
-            for enemy_strat in tqdm(STRATAGEMS, 'Enemy Strats', colour='red'):
+            for enemy_strat in tqdm(STRATAGEMS, f'{strat.__name__}', colour='green', leave=False):
                 wins: int = 0
-                for _ in tqdm(range(NUM_TRIALS), 'Trials', colour='green'):
+                for _ in tqdm(range(NUM_TRIALS), f'VS. {enemy_strat.__name__}', colour='red', leave=False):
                     new_match: Match = Match(experiment.num_blanks, experiment.num_lives, experiment.starting_health, strat(), enemy_strat())
                     wins += 1 if new_match.play() else 0
                 
@@ -74,8 +85,7 @@ def main() -> None:
 
             result_dict['strats'][strat.__name__.lower()]['overall'] = f'{(overall_sum/len(STRATAGEMS)):.3f}'
                 
-        current_time: str = datetime.fromtimestamp(time()).strftime('%H-%M-%S')
-        with open(f'{OUT_DIRECTORY}{day}/experiment-{exp_num}-({current_time}).json', 'w+') as out_file:
+        with open(f'{OUT_DIRECTORY}{day}/{current_time}/experiment-{exp_num}.json', 'w+') as out_file:
             json.dump(result_dict, out_file)
     
 if __name__ == '__main__':
